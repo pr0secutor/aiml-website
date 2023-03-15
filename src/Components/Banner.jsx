50ec51d35186e28b67310eb0193239e92faecc2d
pr0secutor <ifahim3107@gmail.com>
2023-03-16 00:21:25 +0530

    Added Banner animations

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,4 +1,5 @@
 import { Flex, Heading, Text, VStack } from '@chakra-ui/react'
+import { motion } from 'framer-motion'
 import React from 'react'
 import styles from './Banner.module.css'
 
@@ -15,10 +16,18 @@ const Banner = ({title}) => {
       width={'100%'}
       bgColor={'rgba(13, 35, 69, 0.8)'}
       >
-        <Heading id={styles.heading} mt={'20vh'} size={['xl','2xl','3xl',null]}>
+        <Heading id={styles.heading} mt={'20vh'} size={['xl','2xl','3xl',null]}
+        as={motion.div} 
+        initial={{opacity:0,y:60}}
+        animate={{opacity:1,y:0,transition:{duration:0.2}}}
+        >
             {title}
         </Heading>
-        <Text textAlign={'center'} color={'#fff'} p={2} fontSize={['md','lg','2xl',null]}>
+        <Text textAlign={'center'} color={'#fff'} p={2} fontSize={['md','lg','2xl',null]}
+        as={motion.div} 
+        initial={{opacity:0,y:60}}
+        animate={{opacity:1,y:0,transition:{duration:0.2,delay:0.1}}}
+        >
             A non-profit network of college coding clubs and  makers around the world<br/>,for the students ,by the students.
         </Text>
       </VStack>
