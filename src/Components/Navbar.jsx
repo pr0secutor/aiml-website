5abceb28103c8857b584ef4fed31aef1b529ce4f
pr0secutor <ifahim3107@gmail.com>
2023-05-26 15:43:34 +0530

    Minor changes to the navbar.

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Flex, HStack, IconButton, Image, Text } from "@chakra-ui/react";
+import { Flex, HStack, IconButton, Image, Text, useMediaQuery } from "@chakra-ui/react";
 import React from "react";
 import { Link } from "react-router-dom";
 import {
@@ -9,6 +9,10 @@ import {
 } from "react-icons/ai";
 
 const Navbar = () => {
+
+    const [isSmallerThan1000] = useMediaQuery('(max-width:1000px)')
+    const [isSmallerThan768] = useMediaQuery('(max-width:768px)')
+
   return (
     <Flex
       as="nav"
@@ -18,19 +22,19 @@ const Navbar = () => {
       justifyContent="space-evenly"
     >
       <Image height='80px' w='auto' src='https://stickerhuman.hackclub.com/flag.png'/>
-      <HStack w='50%' justifyContent={'space-evenly'}>
+      {!isSmallerThan768&&<HStack w='50%' justifyContent={'space-evenly'}>
         <NavItem text="Events" link="/events" />
         <NavItem text="Members" link="/members" />
         <NavItem text="Projects" link="/projects" />
-        <NavItem text="What we do" link="#" />
+        <NavItem text="What we do" link="/" />
         <NavItem text="Join Us" link="#" />
-      </HStack>
-      <HStack spacing={6}>
+      </HStack>}
+      {!isSmallerThan1000&&<HStack spacing={6}>
         <SocialIcon icon={<AiFillTwitterCircle />} link="https://twitter.com" />
         <SocialIcon icon={<AiFillGithub />} link="https://github.com" />
         <SocialIcon icon={<AiFillLinkedin />} link="https://linkedin.com" />
         <SocialIcon icon={<AiFillInstagram />} link="https://instagram.com" />
-      </HStack>
+      </HStack>}
     </Flex>
   );
 };
